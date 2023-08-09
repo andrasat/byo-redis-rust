dd99089d11342cb23df01a7e0120c0de1f686614
andrasat <andra.satria1@gmail.com>
2023-08-10 01:51:24 +0700

    feat(protocol_parser): added protocol parser for building and parsing messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod client;
+pub mod r#const;
+pub mod protocol_parser;
 pub mod server;
 
 #[cfg(test)]
---
+++ src/protocol_parser.rs
@@ -0,0 +1,54 @@
+use crate::r#const::MAX_MSG;
+use async_std::{io::ReadExt, net::TcpStream};
+use std::io::Error;
+
+const FIRST_PART_SIZE: usize = 4;
+
+pub fn message_builder(msg: String) -> Result<String, Error> {
+    let msg_len = msg.len();
+
+    if msg_len > MAX_MSG {
+        return Err(Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Message is too long",
+        ));
+    }
+
+    let mut msg_len_string = msg_len.to_string();
+    if msg_len_string.len() < FIRST_PART_SIZE {
+        let diff = FIRST_PART_SIZE - msg_len_string.len();
+        for _ in 0..diff {
+            msg_len_string.insert(0, '\0');
+        }
+    }
+
+    let data = format!("{}{}", msg_len_string, msg);
+    return Ok(data);
+}
+
+pub async fn message_parser(mut stream: &TcpStream, mut buf: [u8; MAX_MSG]) -> (String, String) {
+    match stream.read(&mut buf).await {
+        Ok(n) => println!("Read {} bytes", n),
+        Err(e) => panic!("Failed to read from stream: {}", e),
+    };
+
+    let cloned_buf = buf.clone();
+
+    let len_data = cloned_buf[..FIRST_PART_SIZE].to_vec();
+    let msg_len = match String::from_utf8(len_data) {
+        Ok(msg_len) => msg_len.trim_matches('\0').to_string(),
+        Err(e) => panic!("Failed to parse message length: {}", e),
+    };
+    let msg_len_usize = msg_len.trim().parse::<usize>().unwrap();
+
+    let msg_data = cloned_buf[FIRST_PART_SIZE..(FIRST_PART_SIZE + msg_len_usize + 1)].to_vec();
+    let text = match String::from_utf8(msg_data) {
+        Ok(text) => text,
+        Err(e) => panic!("Failed to parse message: {}", e),
+    };
+
+    let pattern: &[_] = &['\r', '\n'];
+    let clean_text = text.trim_matches(pattern).to_string();
+
+    return (msg_len, clean_text);
+}
